a9b6329a4ec16db62f8237ad33e38ddf284ebe18
Sandvich <dfvs314@protonmail.com>
2025-03-18 10:28:15 +0200

    Update Melee_Player.cs
    Vaihtoin E to Fire1

--- Melee_Player.cs
+++ Melee_Player.cs
@@ -16,7 +16,7 @@ public class Melee_Player : MonoBehaviour
 
     void spawn()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.Fire1))
         {
             Instantiate(MeleeProjectile, transform.position, MeleeProjectile.transform.rotation);
         }
